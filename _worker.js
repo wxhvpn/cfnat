8f6638a8edf53aefb28cace384fd1523f570a869
git <41714976+wxhvpn@users.noreply.github.com>
2024-04-07 20:00:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -27,7 +27,7 @@ let addressesnotlsapi = [
 	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 ];
 
-let DLS = 12;//速度下限
+let DLS = 11;//速度下限
 let addressescsv = [
     'https://raw.githubusercontent.com/wxhvpn/d2/main/addressesapi.csv',
     'https://raw.githubusercontent.com/wxhvpn/d2/main/as.csv',
